13ee90fa3cc3c49aded0004bf3ea07043b695f36
Dragos Stefanescu <176727+imhotepper@users.noreply.github.com>
2020-11-16 18:19:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var cors = require("cors");
+app.use(cors());
+
+let todos = [];
+
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.post("/api/todos", (req, res) => {
+  console.log("received: " + JSON.stringify(req.body.title));
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json(req.body);
+});
+
+//create a server object:
+app.get("/", function (req, res) {
+  res.write("Hello World!..."); //write a response to the client
+  res.end(); //end the response
+});
+
+app.listen(8080, function () {
+  console.log("server running on 8080");
+}); //the server object listens on port 8080
